d8da8d25dc55b5c8be1d1204678ff24b86ba03d5
Your Name <prometm@i.ua>
2017-02-14 22:54:57 +0200

    All site tenth

--- impala/urls.py
+++ impala/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list,name='post_list'),
+    url(r'^post_list', views.post_list,name='post_list'),
+    url(r'^specifications', views.specifications,name='specifications'),
+    url(r'^gallery', views.gallery,name='gallery'),
+    url(r'^about', views.about,name='about'),
+    url(r'^store', views.store,name='store'),
 ]
--- impala/views.py
+++ impala/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     return render(request, 'impala/post_list.html', {})
+
+def specifications(request):
+    return render(request, 'impala/specifications.html', {})
+
+def gallery(request):
+    return render(request, 'impala/gallery.html', {})
+
+def about(request):
+    return render(request, 'impala/about.html', {})
+
+def store(request):
+    return render(request, 'impala/store.html', {})
